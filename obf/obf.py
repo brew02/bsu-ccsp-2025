ccb1858da17e1b1bec6b4a45ae85d979b1e9332c
brew02 <brodiea2005@gmail.com>
2025-06-17 09:42:36 -0600

    Cleanup

---
+++ obf/obf.py
@@ -0,0 +1,45 @@
+import pefile
+import sys
+import os
+
+def obf_error(message, usage):
+    print(message)
+    if usage == True:
+        print("Usage: python3 obf.py [file_path | directory_path]")
+    sys.exit(-1)
+
+def obfuscate(file_path):
+    if os.path.isfile(file_path) == False:
+        return False
+    
+    
+
+    return True
+
+argv_len = len(sys.argv)
+
+# We require only two arguments (the first being the name of the script, b2w.py)
+if argv_len != 2:
+    obf_error(f"Invalid number of arguments: {argv_len}", True)
+
+# Support absolute and relative paths
+path = sys.argv[1]
+if os.path.exists == False:
+    path = os.getcwd() + path
+    if os.path.exists == False:
+        obf_error(f"Invalid path: {path}", True)
+    else:
+        print("Using relative path")
+else:
+    print("Using absolute path")
+
+if os.path.isdir(path):
+    # Loop through the directory if that was specified
+    for file_name in os.listdir(path):
+        file_path = os.path.join(path, file_name)
+        b2w(file_path)
+else:
+    b2w(path)
+
+print("Converted to .wav")
+
