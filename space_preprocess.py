697ffe374671bbc86e4f20ea908b96fd0119a924
devyn hubbs <dhubbs2@zagmail.gonzaga.edu>
2025-06-26 10:57:11 -0600

    edit

--- space_preprocess.py
+++ space_preprocess.py
@@ -1,12 +1,13 @@
 import pefile 
 import sys 
 from pathlib import Path
+import monkeyhex 
 
 file_path = Path(sys.argv[1])
 print(f"Filepath: {file_path}")     # DEBUG
 
 pe = pefile.PE(file_path)
-print(pe.FILE_HEADER.Machine)
+print(hex(pe.FILE_HEADER.Machine))
 
 
     
